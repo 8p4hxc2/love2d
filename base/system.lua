8ea0a1b52ef0fda369eae8480a022f3ef3829839
8p4hxc2 <8p4hxc2@gmail.com>
2017-09-23 12:24:45 +0200

    update the parent system class

--- base/system.lua
+++ base/system.lua
@@ -1,14 +1,22 @@
+-- init the system, must be defined on child
+local init = function()
+  error "missing init function on a system"
+end
+
 -- add entity to this system if the blueprint match
 local register = function(self, entity)
   local match = 0
+  local total = #self.blueprint
 
+  -- check if the entity match the blueprint
   for key, value in pairs(self.blueprint) do
     if entity[value] then
       match = match + 1
     end
   end
 
-  if match == self.total then
+  -- yes it match, add it to the entity list
+  if match == total then
     self.entities[#self.entities + 1] = entity
   end
 end
@@ -23,6 +31,7 @@ end
 
 -- exposed to child methods
 return {
+  init = init,
   register = register,
   update = update,
   draw = draw
