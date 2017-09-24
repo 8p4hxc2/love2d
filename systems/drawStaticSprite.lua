15775c3473e422b5f635c7240c966befd1da9655
8p4hxc2 <8p4hxc2@gmail.com>
2017-09-24 20:21:26 +0200

    add system that draw static sprite

---
+++ systems/drawStaticSprite.lua
@@ -0,0 +1,36 @@
+-- inheritance
+local system = require "base/system"
+
+-- alias
+local graphics = love.graphics
+
+-- require
+local cLoader = require "core/loader"
+local cClass = require "core/class"
+
+-- init system
+local init = function(self)
+  self.entities = {}
+  self.blueprint = {"texture", "position"}
+  return self
+end
+
+-- draw all simple sprites
+local draw = function(self)
+  for key, entity in pairs(self.entities) do
+    graphics.draw(cLoader.get(entity.texture), entity.position.x, entity.position.y)
+  end
+end
+
+-- exposed methods
+local methods = {
+  init = init,
+  draw = draw
+}
+
+-- constructor
+local new = function()
+  return cClass.inherit(methods, system)
+end
+
+return {new = new}
