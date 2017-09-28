f4fffd381d8cebfcccddd568dadf3f5ae1af22cb
8p4hxc2 <8p4hxc2@gmail.com>
2017-09-28 12:26:44 +0200

    add core system inheritance and instanciation

--- core/system.lua
+++ core/system.lua
@@ -1,38 +1,53 @@
 -- alias
 local graphics = love.graphics
 
--- vars
-local systems = {}
+-- require
+local cClass = require "core/class"
+
+-- init system
+local init = function(self)
+  self.systems = {}
+  return self
+end
 
 -- add a system
-local add = function(name)
-  systems[name] = require("systems/" .. name).new():init()
+local add = function(self, name)
+  self.systems[name] = require("systems/" .. name).new():init()
 end
 
 -- update all the registered systems
-local update = function(dt)
-  for key, system in pairs(systems) do
+local update = function(self, dt)
+  for key, system in pairs(self.systems) do
     system:update(dt)
   end
 end
 
 -- draw all the registered systems
-local draw = function()
-  for key, system in pairs(systems) do
+local draw = function(self)
+  for key, system in pairs(self.systems) do
     system:draw()
   end
 end
 
 -- register an entity in all the systems
-local registerEntity = function(entity, params)
-  for key, system in pairs(systems) do
+local registerEntity = function(self, entity, params)
+  for key, system in pairs(self.systems) do
     system:register(entity)
   end
 end
 
-return {
+-- exposed methods
+local methods = {
+  init = init,
   add = add,
   update = update,
   draw = draw,
   registerEntity = registerEntity
 }
+
+-- constructor
+local new = function()
+  return cClass.new(methods)
+end
+
+return {new = new}
