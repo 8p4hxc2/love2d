033759ee6ece7f9614d87184204f935875eb6589
8p4hxc2 <8p4hxc2@gmail.com>
2017-09-28 16:02:24 +0200

    handle pause on ui state

--- states/ui.lua
+++ states/ui.lua
@@ -9,16 +9,13 @@ local state = require "base/state"
 local cSystem = require("core/system")
 local cClass = require("core/class")
 
--- entities
-local eButton = require "entities/button"
 
 -- called one time at the creation of the state
 local init = function(self)
   self.systems = cSystem.new():init()
   self.systems:add("drawStaticSprite")
 
-  self.button = eButton.new():init();
-  self.systems:registerEntity(self.button)
+  self.button = self.systems:registerEntity("button")
 end
 
 -- called one time at the destruction of the state
@@ -27,40 +24,44 @@ end
 
 -- called when activating the state
 local play = function (self)
+  self.paused = false
 end
 
 -- called when leaving the state
 local pause = function(self)
+  self.paused = true
 end
 
 -- called everyframe to update the state
 local update = function(self, dt)
-  local button = self.button
-  local x, y = love.mouse.getPosition()
+  if(not self.paused) then
+    local button = self.button
+    local x, y = love.mouse.getPosition()
 
-  if(love.mouse.isDown(1) and not pressed) then
-    pressed = true
-  end
-
-  local isOnButton = button.position.x < x and x < button.position.x + 49 and button.position.y < y and y < button.position.y + 49
-
-  if(not love.mouse.isDown(1)) then
-    if(pressed and alreadypressed) then
-      button.position.y = button.position.y - 4
+    if(love.mouse.isDown(1) and not pressed) then
+      pressed = true
     end
-    pressed = false
-    alreadypressed = false
-  end
 
-  if(pressed and not alreadypressed and isOnButton) then
-    alreadypressed = true
-    button.position.y = button.position.y + 4
-    button.texture = "blue_button12"
-  elseif(not pressed) then
-    button.texture = "blue_button11"
-  end
+    local isOnButton = button.position.x < x and x < button.position.x + 49 and button.position.y < y and y < button.position.y + 49
 
-  self.systems:update(dt)
+    if(not love.mouse.isDown(1)) then
+      if(pressed and alreadypressed) then
+        button.position.y = button.position.y - 4
+      end
+      pressed = false
+      alreadypressed = false
+    end
+
+    if(pressed and not alreadypressed and isOnButton) then
+      alreadypressed = true
+      button.position.y = button.position.y + 4
+      button.texture = "blue_button12"
+    elseif(not pressed) then
+      button.texture = "blue_button11"
+    end
+
+    self.systems:update(dt)
+  end
 end
 
 -- called everyframe to draw the state
