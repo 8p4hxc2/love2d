135a93d8b39995781914e9802dce273e689a1544
8p4hxc2 <8p4hxc2@gmail.com>
2017-09-28 12:26:12 +0200

    fixing core state for inherited state

--- core/state.lua
+++ core/state.lua
@@ -10,7 +10,7 @@ local history = {}
 local switch = function(name)
   -- close current state
   if(activeState) then
-    states[activeState].destroy()
+    states[activeState]:destroy()
     table.remove(activesStates)
     states[activeState] = nil
   end
@@ -19,15 +19,17 @@ local switch = function(name)
   activeState = name
   activesStates[#activesStates + 1] = name
   -- reactivate the state or init
-  states[name] = require("states/" .. name)
-  states[name].init()
-  states[name].play()
+  states[name] = require("states/" .. name).new()
+  states[name]:init()
+  states[name]:play()
 end
 
-local add = function(name)
-  -- pause current state
-  if(activeState) then
-    states[activeState].pause()
+local add = function(name, pause)
+  -- pause currents states
+  if(activesStates and pause) then
+    for key, state in pairs(activesStates) do
+      states[state]:pause()
+    end
   end
 
   -- keep the name of the new current state
@@ -35,14 +37,14 @@ local add = function(name)
   activesStates[#activesStates + 1] = name
 
   -- create the new state
-  states[name] = require("states/" .. name)
-  states[name].init()
-  states[name].play()
+  states[name] = require("states/" .. name).new()
+  states[name]:init()
+  states[name]:play()
 end
 
 local back = function()
   -- pause current state
-  states[activeState].destroy()
+  states[activeState]:destroy()
 
   states[activeState] = nil
   table.remove(activesStates)
@@ -50,18 +52,20 @@ local back = function()
   activeState = activesStates[#activesStates]
 
   -- create the new state
-  states[activeState].play()
+  states[activeState]:play()
 end
 
 -- update the active state
 local update = function(dt)
-  states[activeState].update(dt)
+  for key, state in pairs(activesStates) do
+    states[state]:update(dt)
+  end
 end
 
 -- draw the active state
 local draw = function()
   for key, state in pairs(activesStates) do
-    states[state].draw()
+    states[state]:draw()
   end
 end
 
