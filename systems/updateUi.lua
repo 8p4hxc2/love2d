b78263626cc7f5cde68fc58c252073ed08b822ad
8p4hxc2 <8p4hxc2@gmail.com>
2017-10-03 16:14:42 +0200

    debug updateUi system

--- systems/updateUi.lua
+++ systems/updateUi.lua
@@ -30,7 +30,7 @@ local update = function(self, dt)
       button.canPress.pressed = true
     end
 
-    local isOnButton = button.transform.x < x and x < button.transform.x + 490 and button.transform.y < y and y < button.transform.y + 490
+    local isOnButton = button.transform.x < x and x < button.transform.x + button.transform.width and button.transform.y < y and y < button.transform.y + button.transform.height
 
     if(not mouse.isDown(1)) then
       if(button.canPress.pressed and button.canPress.alreadyPressed) then
@@ -43,9 +43,11 @@ local update = function(self, dt)
     if(button.canPress.pressed and not button.canPress.alreadyPressed and isOnButton) then
       button.canPress.alreadyPressed = true
       button.transform.y = button.transform.y + 4
-      button.texture = "blue_button12"
-    elseif(not pressed) then
-      button.texture = "blue_button11"
+
+      button.canPress.offTexture = button.spriteRenderer.texture
+      button.spriteRenderer.texture = button.canPress.onTexture or button.spriteRenderer.texture
+    elseif(not button.canPress.pressed) then
+      button.spriteRenderer.texture = button.canPress.offTexture or button.spriteRenderer.texture
     end
   end
 end
