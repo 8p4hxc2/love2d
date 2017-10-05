7d43886c5a35b0dc80059e6f1a92359a802f5fdf
8p4hxc2 <8p4hxc2@gmail.com>
2017-10-05 22:13:32 +0200

    generating map from tiled

--- main.lua
+++ main.lua
@@ -8,8 +8,12 @@ local timer = love.timer
 -- core
 local cState = require "core/state"
 local cLoader = require "core/loader"
+local cameraX = 800
+local cameraY = 2800
 
 function love.load()
+  love.window.setMode(1280, 720)
+
   -- assets loading
   cLoader.getFiles("assets")
 
@@ -22,19 +26,45 @@ function love.load()
   -- add ui state
   --cState.add("ui")
   map = require("assets/darktober")
+
+  quad = love.graphics.newQuad(400, 0, 400, 400, 3200, 800)
 end
 
 function love.update(dt)
   cState.update(dt)
+  if keyboard.isDown("right") then
+    cameraX = cameraX + 10
+  end
+
+  if keyboard.isDown("left") then
+    cameraX = cameraX - 10
+  end
+
+  if keyboard.isDown("up") then
+    cameraY = cameraY - 10
+  end
+
+  if keyboard.isDown("down") then
+    cameraY = cameraY + 10
+  end
 end
 
 function love.draw()
   cState.draw()
-  graphics.print("Current FPS: " .. timer.getFPS(), 10, 10)
-
+  graphics.print("Current FPS: " .. timer.getFPS() .. map.layers[1].data[31] .. map.layers[1].data[31]-8, 10, 10)
+  graphics.translate(-cameraX, -cameraY)
   for x = 1, 10 do
     for y = 1, 10 do
-      graphics.print("" .. map.tilesets[0].type, 10, 10)
+      quad =
+        love.graphics.newQuad(
+        (map.layers[1].data[x+(y-1)*10]-1) * 400,
+        0,
+        400,
+        400,
+        3200,
+        800
+      )
+      graphics.draw(cLoader.get(map.tilesets[1].name), quad, x*400, y*400)
     end
   end
 end
