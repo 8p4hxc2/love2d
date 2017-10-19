dc259ffc085639826e38963fbe86982e979a17f1
8p4hxc2 <8p4hxc2@gmail.com>
2017-10-19 16:27:33 +0200

    add animatedSprite to ennemy

--- entities/enemy.lua
+++ entities/enemy.lua
@@ -3,25 +3,23 @@ local entity = require "base/entity"
 
 -- alias
 local physics = love.physics
-local graphics = love.graphics
 
 -- require
 local cClass = require "core/class"
 
 -- init enemy
-local init = function(self, config)
+local init =
+  function(self, config)
   self.enn = true
 
   self:add("transform", {x = 100, y = 300, width = 200, height = 200})
   self:add("canPress")
   --  self:add("spriteRenderer", {texture = "sorcererVillain"})
 
-  local frames = {}
-
-  for i = 1, 10 do
-    frames[#frames + 1] = graphics.newQuad((i - 1) * 200, 0, 200, 200, 2000, 200)
-  end
-  self:add("animatedSprite", {frames = frames})
+  self:add(
+    "animatedSprite",
+    {frameWidth = 200, frameHeight = 200, sheetWidth = 2000, sheetHeight = 200, frameCount = 10, frameDuration = 0.1}
+  )
   self:add("rigidBody", {entity = self, world = config})
 
   return self
