dc6f8aff19aee9ef90cd57c4a73541c32035652e
8p4hxc2 <8p4hxc2@gmail.com>
2017-10-19 16:27:06 +0200

    add props to animatedSprite component

--- components/animatedSprite.lua
+++ components/animatedSprite.lua
@@ -1,9 +1,20 @@
+-- alias
+local graphics = love.graphics
+
 -- init the component
 local init = function(self, config)
   config = config or {}
 
   -- define the properties of the component
   self.frames = config.frames or {}
+
+  for i = 1, config.frameCount do
+    self.frames[#self.frames + 1] = graphics.newQuad((i - 1) * config.frameWidth, 0, config.frameWidth, config.frameHeight, config.sheetWidth, config.sheetHeight)
+  end
+
+  self.currentFrame = config.currentFrame or 1
+  self.frameDuration = config.frameDuration or 0.1
+  self.lastUpdate = 0
 
   return self
 end
