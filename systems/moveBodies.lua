98cbdd187d7b8ab1d28ca6c3e58a782c508f07a9
8p4hxc2 <8p4hxc2@gmail.com>
2017-10-24 16:42:03 +0200

    add new system

---
+++ systems/moveBodies.lua
@@ -0,0 +1,42 @@
+-- inheritance
+local system = require "base/system"
+
+-- alias
+
+-- require
+local cClass = require "core/class"
+
+-- init system
+local init = function(self)
+  self.entities = {}
+  self.blueprint = {"rigidBody", "transform"}
+  return self
+end
+
+-- draw the system
+local draw = function(self)
+  for key, entity in pairs(self.entities) do
+  end
+end
+
+-- update the system
+local update = function(self, dt)
+  for key, entity in pairs(self.entities) do
+    entity.transform.x = entity.rigidBody.body:getX()
+    entity.transform.y = entity.rigidBody.body:getY()
+  end
+end
+
+-- exposed methods
+local methods = {
+  init = init,
+  update = update,
+  draw = draw
+}
+
+-- constructor
+local new = function()
+  return cClass.inherit(methods, system)
+end
+
+return {new = new}
