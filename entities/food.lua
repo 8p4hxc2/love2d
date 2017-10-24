ee5009cb5f7d3a3feb4cf6c29b2c7237432ca282
8p4hxc2 <8p4hxc2@gmail.com>
2017-10-24 16:40:56 +0200

    change food position

--- entities/food.lua
+++ entities/food.lua
@@ -9,10 +9,10 @@ local cClass = require "core/class"
 
 -- init enemy
 local init = function(self, config)
-  self:add("transform", {x = 700, y = 50, width = 32, height = 32})
+  self:add("transform", {x = 1000, y = 2800, width = 32, height = 32})
   self:add("canPress")
   self:add("spriteRenderer", {texture = "food"})
-  self:add("rigidBody", {entity = self, world = config, type = "static"})
+  self:add("rigidBody", {entity = self, world = config})--, type = "static"})
   self:add("rectangleCollision", {entity = self})
 
   return self
