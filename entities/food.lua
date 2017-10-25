1b361f4acb4b550fb6ae70303204b9b547a53409
8p4hxc2 <8p4hxc2@gmail.com>
2017-10-25 22:44:12 +0200

    change food texture

--- entities/food.lua
+++ entities/food.lua
@@ -9,9 +9,9 @@ local cClass = require "core/class"
 
 -- init enemy
 local init = function(self, config)
-  self:add("transform", {x = 1000 + love.math.random(500), y = 2800 - love.math.random(500), width = 32, height = 32})
+  self:add("transform", {x = 1000 + love.math.random(2500), y = 2800 - love.math.random(2500), width = 49, height = 49})
   self:add("canPress")
-  self:add("spriteRenderer", {texture = "food"})
+  self:add("spriteRenderer", {texture = "green_button10"})
   self:add("rigidBody", {entity = self, world = config})--, type = "static"})
   self:add("rectangleCollision", {entity = self})
 
